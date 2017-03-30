8a7ddfa170373fe9c77d228b0b36659aa42370ec
sergeikostin5 <sergei.s.kostin@gmail.com>
2017-03-30 10:40:17 -0700

    added user model

---
+++ src/main/java/com/sergeikostin/model/UserRepository.java
@@ -0,0 +1,10 @@
+package com.sergeikostin.model;
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+}
